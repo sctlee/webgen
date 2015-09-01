c3ebb1e8b7e0f86649f5b89b7835903bd30b3b64
Secret <haichuang221@163.com>
2015-09-02 00:12:20 +0800

    use cobra CLI && some modify

--- src/webgen/webgen.go
+++ src/webgen/webgen.go
@@ -2,30 +2,73 @@ package main
 
 import (
 	"fmt"
-	"os"
+	// "os"
 
 	"command"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
 	fmt.Println("Hello Secret!")
-	args := os.Args
+	// args := os.Args
 
 	// if args == nil || len(args) < 2 {
 	// 	fmt.Println("Usage")
 	// 	return
 	// }
 
-	switch args[1] {
-	case "init":
-		command.Init()
-	case "build":
-		command.Build()
-	case "push":
-		command.Push()
-	case "reset":
-		command.Reset()
-	default:
-		fmt.Println("Usage")
+	// switch args[1] {
+	// case "init":
+	// 	command.Init()
+	// case "build":
+	// 	command.Build()
+	// case "push":
+	// 	command.Push()
+	// case "reset":
+	// 	command.Reset()
+	// default:
+	// 	fmt.Println("Usage")
+	// }
+	var cmdInit = &cobra.Command{
+		Use:   "init",
+		Short: "init",
+		Long:  `init`,
+		Run: func(cmd *cobra.Command, args []string) {
+			command.Init()
+		},
 	}
+
+	var cmdBuild = &cobra.Command{
+		Use:   "build",
+		Short: "build",
+		Long:  `build`,
+		Run: func(cmd *cobra.Command, args []string) {
+			command.Build()
+		},
+	}
+
+	var cmdPush = &cobra.Command{
+		Use:   "push",
+		Short: "push",
+		Long:  `push`,
+		Run: func(cmd *cobra.Command, args []string) {
+			command.Push()
+		},
+	}
+
+	var cmdReset = &cobra.Command{
+		Use:   "reset",
+		Short: "build",
+		Long:  `build`,
+		Run: func(cmd *cobra.Command, args []string) {
+			command.Reset()
+		},
+	}
+	// cmdHaha.Flags().IntVarP(&t, "type", "t", 1, "type hahaha")
+
+	var rootCmd = &cobra.Command{Use: "webgen"}
+	rootCmd.AddCommand(cmdInit, cmdBuild, cmdPush, cmdReset)
+	rootCmd.Execute()
+
 }
